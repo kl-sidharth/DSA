42684c59a287da1a419b4c35b5f2db35d69308b6
Sidharth <sidharthkujala@gmail.com>
2026-03-26 15:59:22 +0530

    bubbleSort.java

--- Sorting Algorithms/bubbleSort.java
+++ Sorting Algorithms/bubbleSort.java
@@ -4,7 +4,7 @@ class Main {
         for(int i=n-1;i>=1;i--){
             for(int j =0;j<i;j++){
                 if(arr[j]>arr[j+1]){
-                int temp = arr[j];
+                int temp = arr[j]; 
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
                 }
